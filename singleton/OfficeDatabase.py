d09a8b73e8ea120f050cf2071f516e64e087ea88
w3villa-Akshat <Akshat.gupta@W3villa.com>
2025-11-15 15:58:04 +0530

    update

--- singleton/OfficeDatabase.py
+++ singleton/OfficeDatabase.py
@@ -1,18 +1,3 @@
-# class OfficeDatabase:
-#     __instance = None
-
-#     def __init__(self):
-#         if OfficeDatabase.__instance is not None:
-#             raise Exception("This class is a Singleton!")
-#         print("Database Connected")
-
-#     @staticmethod
-#     def get_instance():
-#         if OfficeDatabase.__instance is None:
-#             OfficeDatabase.__instance = OfficeDatabase()
-#         return OfficeDatabase.__instance
-
-
 class OfficeDatabase:
     __instance = None 
     
